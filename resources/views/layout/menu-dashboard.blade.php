98c69efc554d8278f328b72e9c23dec64af88e4d
zeonita <gianovitrizeonita@gmail.com>
2022-09-17 16:29:17 +0700

    add logout and menu kategori

--- resources/views/layout/menu-dashboard.blade.php
+++ resources/views/layout/menu-dashboard.blade.php
@@ -8,11 +8,17 @@
                 Produk
             </a>
         </li>
+        <li class="mb-1">
+            <a href="{{ route('category.index') }}" class="d-flex align-items-center py-2 link-light text-decoration-none">
+                Kategori
+            </a>
+        </li>
         <li class="border-top my-3"></li>
         <li class="mb-1">
-            <a href="/" class="d-flex align-items-center py-2 link-light text-decoration-none">
-                Logout
-            </a>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="border border-0 bg-transparent text-light p-0" href="{{ route('logout') }}" role="button">Logout</button>
+            </form>
         </li>
     </ul>
   </div>
