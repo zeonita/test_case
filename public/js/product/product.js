0c60ce3c5853e2e333b569e0a32b20f474e39641
zeonita <gianovitrizeonita@gmail.com>
2022-09-17 22:43:49 +0700

    load product datatable

--- public/js/product/product.js
+++ public/js/product/product.js
@@ -1,3 +1,80 @@
 $(document).ready( function () {
-    $('#table-product').DataTable();
+    $('#table-product').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            'url': urlDT,
+            'type': 'GET',
+            'dataSrc': function (response) {
+                return response.data;
+            }
+        },
+        "columnDefs": [
+            {
+                "targets": 0,
+                "name": null,
+                "render": function (data, type, row) {
+                    return row.sku
+                }
+            },
+            {
+                "targets": 1,
+                "name": null,
+                "render": function (data, type, row) {
+                    return '<img src="' + row.detail.image + '" class="img w-25 h-25 border rounded">'
+                }
+            },
+            {
+                "targets": 2,
+                "name": null,
+                "render": function (data, type, row) {
+                    return row.name
+                }
+            },
+            {
+                "targets": 3,
+                "name": null,
+                "render": function (data, type, row) {
+                    return row.category.name
+                }
+            },
+            {
+                "targets": 4,
+                "name": null,
+                "render": function (data, type, row) {
+                    return row.detail.price
+                }
+            },
+            {
+                "targets": 5,
+                "name": null,
+                "render": function (data, type, row) {
+                    return '<a href="produk/' + row.id + '" class="btn btn-success me-2">Edit</a>' +
+                        '<a href="produk/' + row.id + '" class="btn btn-danger remove-item">Hapus</a>'
+                }
+            }
+        ]
+    });
+        
+    $('#table-product').on('click', '.remove-item', function (event) {
+        var productId = $(this).data('id');
+        var url = $(this).attr('href');
+        event.preventDefault();
+        if (confirm('Apakah anda yakin ingin menghapus kategori ini?')) {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (response) {
+                    $('.alert-success').addClass('d-none');
+                    $('#table-product').DataTable().ajax.reload(null, false);
+                    $('#delete-alert').removeClass('d-none');
+                }
+            });
+        }
+    });
 } );
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -12,7 +12,7 @@
         <table id="table-product" class="table">
             <thead>
                 <tr>
-                    <th>Id</th>
+                    <th>SKU</th>
                     <th>Foto</th>
                     <th>Nama Produk</th>
                     <th>Kategori</th>
@@ -21,35 +21,15 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th>1</th>
-                    <td>
-                        <div class="pb-3 w-100">
-                            <img src="https://picsum.photos/100/100" class="img rounded" />
-                        </div>
-                    </td>
-                    <td>Ransel</td>
-                    <td>Tas</td>
-                    <td>Rp 300.000</td>
-                    <td><button class="btn btn-success">Edit</button></td>
-                </tr>
-                <tr>
-                    <th>2</th>
-                    <td>
-                        <div class="pb-3 w-100">
-                            <img src="https://picsum.photos/100/100" class="img rounded" />
-                        </div>
-                    </td>
-                    <td>Kemaja</td>
-                    <td>Baju</td>
-                    <td>Rp 200.000</td>
-                    <td><button class="btn btn-success">Edit</button></td>
-                </tr>
+                
             </tbody>
         </table>
     </div>
 @endsection
 
 @push('scripts')
+    <script>
+        var urlDT = "{{ route('api.product-list') }}";
+    </script>
     <script src="{{ asset('js/product/product.js') }}" defer></script>
 @endpush
